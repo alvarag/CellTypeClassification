04dd8f4100e33bd631eed6b12d03a9095c704000
alvarag <alvarag@ubu.es>
2020-05-11 14:24:45 +0200

    Adjusted different radius for different transformations

--- CellAugmentation/Augment.m
+++ CellAugmentation/Augment.m
@@ -11,14 +11,14 @@ numTrans = 5;      %number of translations per each image
 
 %theta = 54;       %angle to rotate
 filtNumR = 4;      %smoothing parameter for gaussian filter
-patchRadius = 20;  %radius of boundary between the original image and the
+patchRadiusR = 20;  %radius of boundary between the original image and the
 %cropped added portions of the image (e.g. top row moved to bottom row) for
 %smoothing
 
 %rowT = 3;         %rows translated
 %colT = 2;         %cols translated
 filtNumT = 0.5;    %smoothing parameter for gaussian filter in patch
-patchRadius = 3;%radius of boundary between the original image and the
+patchRadiusT = 3;%radius of boundary between the original image and the
 %cropped added portions of the image (e.g. top row moved to bottom row) for
 %smoothing
 
@@ -34,13 +34,13 @@ for i = 1 : numFiles
     
     % performs rotations
     for j = 1 : numRots
-        rot1 = rotateIm(img, ceil(rand()*40), filtNumR, patchRadius);
+        rot1 = rotateIm(img, ceil(rand()*40), filtNumR, patchRadiusR);
         imwrite(rot1, strcat(char(name), '_r', num2str(j), '.jpg'), 'jpg');
     end
 
     % performs translations
     for j = 1 : numTrans
-        trans1 = transIm(img, 1+ceil(rand()*4), 1+ceil(rand()*4), filtNumT, patchRadius);
+        trans1 = transIm(img, 1+ceil(rand()*4), 1+ceil(rand()*4), filtNumT, patchRadiusT);
         imwrite(trans1, strcat(char(name), '_t', num2str(j), '.jpg'), 'jpg');
     end
 end
